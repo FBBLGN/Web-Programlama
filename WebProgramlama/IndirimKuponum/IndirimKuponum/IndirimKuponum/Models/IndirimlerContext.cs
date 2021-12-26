8933b0260bb58563a758ed67c6d38d203219c735
FBBLGN <58918860+FBBLGN@users.noreply.github.com>
2021-12-26 05:34:45 +0300

    .

--- WebProgramlama/IndirimKuponum/IndirimKuponum/IndirimKuponum/Models/IndirimlerContext.cs
+++ WebProgramlama/IndirimKuponum/IndirimKuponum/IndirimKuponum/Models/IndirimlerContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
+using IndirimKuponum.Data;
 
 namespace IndirimKuponum.Models
 {
@@ -15,5 +16,7 @@ namespace IndirimKuponum.Models
         public DbSet<Indirimler> Indirim { get; set; }
         public DbSet<Kategori> Kategoriler { get; set; }
         public DbSet<Yorum> Yorumlar { get; set; }
+
+
     }
 }
